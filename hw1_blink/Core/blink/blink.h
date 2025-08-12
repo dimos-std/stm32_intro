37b2d6800c8a63de475d6578346ba74b35611daa
dimos-std <dimpost58@mail.ru>
2025-08-12 10:07:03 +0300

    first commit

---
+++ hw1_blink/Core/blink/blink.h
@@ -0,0 +1,16 @@
+/*
+ * blink.h
+ *
+ *  Created on: Aug 11, 2025
+ *      Author: master
+ */
+
+#ifndef BLINK_H_
+#define BLINK_H_
+
+#include "main.h"
+
+void blink_led_om_board(void);
+
+
+#endif /* BLINK_H_ */
